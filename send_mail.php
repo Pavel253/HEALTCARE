1e7b7ec177e1b31690e1670f4e95c79e4e56094f
Pavel253 <pavelszvetkov@gmail.com>
2022-08-18 14:30:39 +0300

    second commit

---
+++ send_mail.php
@@ -0,0 +1,40 @@
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require "PHPMailer/src/Exception.php";
+    require "PHPMailer/src/PHPMailer.php";
+
+    $mail = new PHPMailer(true);
+	
+    $mail->CharSet = "UTF-8";
+    $mail->IsHTML(true);
+
+    $name = $_POST["name"];
+	$phone = $_POST["phone"];
+	$email_template = "template_mail.html";
+
+    $body = file_get_contents($email_template);
+	$body = str_replace('%name%', $name, $body);
+	$body = str_replace('%phone%', $phone, $body);
+
+
+    $mail->addAddress("pavelszvetkov@gmail.com");   // Здесь введите Email, куда отправлять
+    
+    $mail->Subject = "[Заявка с формы]";
+    $mail->MsgHTML($body);
+
+    if (!$mail->send()) {
+        $message = "Ошибка отправки";
+    } else {
+        $message = "Данные отправлены!";
+    }
+	
+	$response = ["message" => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
+
+
+?>
